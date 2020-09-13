64ea7426a811e69e552630bf9cf15dfd9c658b70
Vinith611 <70832347+Vinith611@users.noreply.github.com>
2020-09-13 14:27:40 +0530

    Add files via upload

---
+++ Day-3.py
@@ -0,0 +1,84 @@
+ASSIGNMENT - 1:-
+
+for alt in range(1000,10000):
+    alt=int(input("Enter the Altitude:"))
+    if alt==1000:
+        print("Safe to Land")
+    elif alt>1000 and alt<5000:
+        print("Bring down to 1000")
+    else:
+        print("Turn Around")
+    
+Output:
+
+Enter the Altitude:1000
+Safe to Land
+Enter the Altitude:4500
+Bring down to 1000
+Enter the Altitude:6500
+Turn Around
+Enter the Altitude:
+
+
+ASSIGNMENT - 2 :-
+
+
+for Number in range (1, 200):
+    count = 0
+    for i in range(2, (Number//2 + 1)):
+        if(Number % i == 0):
+            count = count + 1
+            break
+
+    if (count == 0 and Number != 1):
+        print(" %d" %Number, end = '\n ')
+
+
+Output:
+
+  2
+  3
+  5
+  7
+  11
+  13
+  17
+  19
+  23
+  29
+  31
+  37
+  41
+  43
+  47
+  53
+  59
+  61
+  67
+  71
+  73
+  79
+  83
+  89
+  97
+  101
+  103
+  107
+  109
+  113
+  127
+  131
+  137
+  139
+  149
+  151
+  157
+  163
+  167
+  173
+  179
+  181
+  191
+  193
+  197
+  199
